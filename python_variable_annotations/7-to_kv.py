37f208af3bc43d772040bd1b8eeb1ae03c8e3a3c
flockwood <flockwood@live.com>
2025-07-30 11:29:27 -0400

    feat: add to_kv function with tuple and union type annotations

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module containing a type-annotated to_kv function"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Return a tuple with string k and the square of v"""
+    return (k, v * v)
