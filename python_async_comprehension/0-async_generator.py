68d12c00b13517c57947dc8a3067e9e3dfb90ab3
flockwood <flockwood@live.com>
2025-07-30 12:49:37 -0400

    feat: implement async_generator coroutine with random number yielding

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module containing an async generator coroutine"""
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """Async generator that yields random numbers between 0 and 10"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
