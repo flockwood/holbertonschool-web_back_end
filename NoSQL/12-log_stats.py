cd2b73583cdcc704d708e878f8e8ece04bdad8bc
flockwood <flockwood@live.com>
2025-08-13 10:15:28 -0400

    made file executable

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -3,24 +3,29 @@
 from pymongo import MongoClient
 
 
-if __name__ == "__main__":
+def print_nginx_stats():
+    """Print statistics about nginx logs"""
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
     
-    # Total number of logs
-    total_logs = nginx_collection.count_documents({})
-    print("{} logs".format(total_logs))
+    # Total logs
+    total = nginx_collection.count_documents({})
+    print("{} logs".format(total))
     
-    # Methods statistics
+    # Methods
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
         count = nginx_collection.count_documents({"method": method})
         print("\tmethod {}: {}".format(method, count))
     
-    # Status check count
-    status_check = nginx_collection.count_documents({
+    # Status checks
+    status_checks = nginx_collection.count_documents({
         "method": "GET",
         "path": "/status"
     })
-    print("{} status check".format(status_check))
+    print("{} status check".format(status_checks))
+
+
+if __name__ == "__main__":
+    print_nginx_stats()
