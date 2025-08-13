236c0b2afd733755dc0c74ded4723ba8bdf1f8e3
flockwood <flockwood@live.com>
2025-08-13 09:58:01 -0400

    fixed issues with the name and main syntax

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -9,16 +9,16 @@ if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     db = client.logs
     collection = db.nginx
-
+    
     total_logs = collection.count_documents({})
     print(f"{total_logs} logs")
-
+    
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
         count = collection.count_documents({"method": method})
-        print(f"	method {method}: {count}")
-
+        print(f"\tmethod {method}: {count}")
+    
     status_check_count = collection.count_documents(
         {"method": "GET", "path": "/status"}
     )
