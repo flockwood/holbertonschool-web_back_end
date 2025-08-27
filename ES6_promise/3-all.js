81ffb00ad27d42602678c8c6868a5241b2188ef6
flockwood <flockwood@live.com>
2025-08-27 10:27:07 -0400

    fix: use string concatenation instead of template literals in handleProfileSignup

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,8 +3,9 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
     return Promise.all([uploadPhoto(), createUser()])
       .then((results) => {
-        const [photo, user] = results;
-        console.log('${photo.body} ${user.firstName} ${user.lastName}');
+        const photo = results[0];
+        const user = results[1];
+        console.log(photo.body + ' ' + user.firstName + ' ' + user.lastName);
       })
       .catch(() => {
         console.log('Signup system offline');
