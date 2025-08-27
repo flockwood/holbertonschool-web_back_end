5ad59bda6cdcdd4621076f364443085f47610ab6
flockwood <flockwood@live.com>
2025-08-27 10:56:25 -0400

    fix: correct parameter name in map callback

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -7,7 +7,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
         uploadPhoto(fileName)
     ])
         .then((results) => {
-            return results.map((results) => ({
+            return results.map((result) => ({
                 status: result.status,
                 value: result.status == 'fulfilled' ? result.value : result.reason
             }));
